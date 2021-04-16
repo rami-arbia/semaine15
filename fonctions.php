88afad8e8e32803af26565b19f4b6b0d9e6f422e
Arbia rami <rami.tohem@gmail.com>
2021-04-16 10:18:00 +0200

    depot hebdomadaire incomplet

---
+++ fonctions.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+
+$addition = 2 + 4;
+$soustraction = 6 - 2;
+$multiplication = 5 * 3;
+$division = 15 / 3;
+echo "Addition: 2 + 4 = ".$addition."<br />";
+echo "Soustraction: 6 - 2 = ".$soustraction."<br />";
+echo "Multiplication:  5 * 3 = ".$multiplication."<br />";
+echo "Division: 15 / 3 = ".$division."<br />";
+
+?>
+
+</body>
+</html>
